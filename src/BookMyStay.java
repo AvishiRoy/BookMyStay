7eaa073007a4c5c0db131a46bc53f1f243935168
Avishi Roy <avishi.feb28@gmail.com>
2026-03-10 15:46:06 +0530

    feature/UC1

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -1,4 +1,13 @@
-package PACKAGE_NAME;
-
 public class BookMyStay {
+
+    public static void main(String[] args) {
+
+        System.out.println("=================================");
+        System.out.println("Welcome to Book My Stay");
+        System.out.println("Hotel Booking System v1.0");
+        System.out.println("Application started successfully");
+        System.out.println("=================================");
+
+    }
+
 }
