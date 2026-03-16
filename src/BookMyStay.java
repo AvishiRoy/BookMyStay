24721509d019ddbc926c92d636ec796daa5c14da
dakshchaudhary16 <dakshdagur953@gmail.com>
2026-03-17 01:11:36 +0530

    feature/UC2

--- src/BookMyStay.java
+++ src/BookMyStay.java
@@ -3,11 +3,17 @@ public class BookMyStay {
     public static void main(String[] args) {
 
         System.out.println("=================================");
-        System.out.println("Welcome to Book My Stay");
-        System.out.println("Hotel Booking System v1.0");
-        System.out.println("Application started successfully");
+        System.out.println("Book My Stay - Room Availability");
         System.out.println("=================================");
 
-    }
+        // Static room availability
+        int singleRoomAvailable = 10;
+        int doubleRoomAvailable = 5;
+        int suiteRoomAvailable = 2;
 
+        System.out.println("Single Rooms Available : " + singleRoomAvailable);
+        System.out.println("Double Rooms Available : " + doubleRoomAvailable);
+        System.out.println("Suite Rooms Available  : " + suiteRoomAvailable);
+
+    }
 }
